3a2c85875d7b5a23fd130ad1b07b1e5a0d4563ab
TACAMP <112141419+TACAMP@users.noreply.github.com>
2023-01-02 16:35:05 +0900

    1-1(2)

--- 1-1.java
+++ 1-1.java
@@ -1,5 +1,18 @@
 public class Main {
     public static void main ( String[ ] args ) {
         System.out.println ( "Java最高！");
+        System.out.println ( "Rubyの次に学習中の言語です。");
+        System.out.println ( 5 + 5);
+
+        int x;
+        x = 6;
+        System.out.println ( x * x * 3.14) ;
     }
 }
+
+// 以上が例となる
+
+// Javaは①ソースコードの作成②コンパイル③実行の流れである
+// 「ソースコードの作成」＝　Javaが定める文法に従って、人間が可読できるようにプログラムを記述すること。
+// 「コンパイル」＝　ソースコードに対して処理を行い、バイトコードに変換すること。
+// 「実行」＝　コンパイル後。インタプリタでバイトコードの指示をする
