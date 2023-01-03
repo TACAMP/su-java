84bbc97ccae13dd189847b62c3b15cbdf72b61f0
TACAMP <112141419+TACAMP@users.noreply.github.com>
2023-01-03 22:18:13 +0900

    1-3(1)

--- 1-3.java
+++ 1-3.java
@@ -1,4 +1,13 @@
 // 「mainメソッド」＝　「public static void main (string [] args)」←このmainの部分がmainメソッドと呼ばれる所以
 // mainメソッドの中には文(statement)を順番にかいていく。
 // プログラミングは上から順番に処理されていく
-// ⚠︎文末には必ず「;」をつけることを忘れないで！
+// ⚠︎文末には必ず「;」をつけることを忘れないで!
+public class Main{
+    public static void main( String [] args){
+        System.out.println("Hello World!");
+        System.out.println( 5*5 );
+        int x;
+        x = 3;
+        System.out.println( x * x);
+    }
+}
