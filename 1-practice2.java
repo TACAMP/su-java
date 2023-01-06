312feca073397545b403c6efa634e6ef80a32590
TACAMP <112141419+TACAMP@users.noreply.github.com>
2023-01-06 12:31:48 +0900

    1-practice2.java

--- 1-practice2.java
+++ 1-practice2.java
@@ -1,5 +1,5 @@
-boolean a = true;
-cher b = '駆';
-double c = 3.14;
-long d = 314159265853979L;
-String e = "ミナトの攻撃!敵に15ポイントのダメージを与えた。"
+// boolean a = true;
+// cher b = '駆';
+// double c = 3.14;
+// long d = 314159265853979L;
+// String e = "ミナトの攻撃!敵に15ポイントのダメージを与えた。"
