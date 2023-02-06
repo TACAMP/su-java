41dbf26c4b66c53f9e645354b09fd85aa558da16
TACAMP <112141419+TACAMP@users.noreply.github.com>
2023-02-06 23:25:40 +0900

    fix fizzbuzz method

--- Sujava/alh.java
+++ Sujava/alh.java
@@ -1,12 +1,16 @@
 public class Fizzbuzz{
     public static void main( String [] args ){
+        fizzbuzz();
+        }
+    
+    public static void fizzbuzz(){
         for (int i=1;i <= 100;i++){
             if (i % 15 == 0){
                 System.out.println("FizzBuzz");
             }else if(i % 3 == 0){
                 System.out.println("Fizz");
             }else if(i % 5 == 0){
-                System.out.println("Buzzです");
+                System.out.println("Buzz");
             }else{
                 System.out.println(i);
             }
